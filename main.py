367dbed83cb57ea1fb34d456163067e53578a102
Michael Eakins <michael.eakins@bullhorn.com>
2024-07-31 14:03:37 -0400

    Loads user pools

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+import os
 
 def get_cognito_users(user_pool_id):
+    AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
+    print(AWS_ACCESS_KEY_ID)
+    print(AWS_SECRET_ACCESS_KEY)
     # Initialize a Cognito Identity Provider client
     client = boto3.client('cognito-idp')
+    paginator = client.get_paginator("list_user_pools")
 
-    try:
-        # Paginate through all users in the user pool
-        paginator = client.get_paginator('list_users')
-        page_iterator = paginator.paginate(UserPoolId=user_pool_id)
+    # Create a PageIterator from the paginator
+    page_iterator = paginator.paginate(MaxResults=10)
 
-        for page in page_iterator:
-            for user in page['Users']:
-                username = user['Username']
-                creation_date = user['UserCreateDate']
-                print(f'Username: {username}, Creation Date: {creation_date}')
+    # Initialize variables for pagination
+    user_pools = []
 
-    except NoCredentialsError:
-        print("Credentials not available")
-    except PartialCredentialsError:
-        print("Incomplete credentials")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # Handle pagination
+    for page in page_iterator:
+        user_pools.extend(page.get("UserPools", []))
+
+    # Print the list of user pools
+    print("User Pools for the account:")
+    if user_pools:
+        for pool in user_pools:
+            print(f"Name: {pool['Name']}, ID: {pool['Id']}, CreationDate: {pool["CreationDate"]}")
+    else:
+        print("No user pools found.")
 
 if __name__ == '__main__':
-    # Replace with your Cognito User Pool ID
-    user_pool_id = 'us-east-1_example'
+    user_pool_id = 'us-west2'
     get_cognito_users(user_pool_id)
