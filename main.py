eefd4451e8deb46df92e93c7e9b57c5d8364b159
Michael Eakins <michael.eakins@protonmail.com>
2024-07-31 14:27:32 -0400

    Fix list_user_pools

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
-import os
 
-def get_cognito_users(user_pool_id):
-    AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
-    AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
-    print(AWS_ACCESS_KEY_ID)
-    print(AWS_SECRET_ACCESS_KEY)
-    # Initialize a Cognito Identity Provider client
-    client = boto3.client('cognito-idp')
-    paginator = client.get_paginator("list_user_pools")
+def list_user_pools(client):
+    try:
+        user_pools = []
+        response = client.list_user_pools(MaxResults=60)
+        user_pools.extend(response['UserPools'])
 
-    # Create a PageIterator from the paginator
-    page_iterator = paginator.paginate(MaxResults=10)
+        while 'NextToken' in response:
+            response = client.list_user_pools(MaxResults=60, NextToken=response['NextToken'])
+            user_pools.extend(response['UserPools'])
 
-    # Initialize variables for pagination
-    user_pools = []
-
-    # Handle pagination
-    for page in page_iterator:
-        user_pools.extend(page.get("UserPools", []))
-
-    # Print the list of user pools
-    print("User Pools for the account:")
-    if user_pools:
-        for pool in user_pools:
-            print(f"Name: {pool['Name']}, ID: {pool['Id']}, CreationDate: {pool["CreationDate"]}")
-    else:
-        print("No user pools found.")
+        return user_pools
+    except Exception as e:
+        print(f"An error occurred while listing user pools: {e}")
+        return []
 
 def list_users_in_pool(client, user_pool_id):
     try:
